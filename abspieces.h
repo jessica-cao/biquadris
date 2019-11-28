5202e0478b8b45e03a06f5960596db40612eba09
yjjcao <yjjcao@student.cs.uwaterloo.ca>
2019-11-27 22:25:18 -0500

    corrected virtual func errors

--- abspieces.h
+++ abspieces.h
@@ -2,10 +2,9 @@
 
 class AbsPieces {
     Info data;
-    void rotate_cw() = 0;
-    void rotate_ccw() = 0;
-    void move_l() = 0;
-    void move_d() = 0;
-    void move_r() = 0;
-}
-
+    virtual void rotate_cw() = 0;
+    virtual void rotate_ccw() = 0;
+    virtual void move_l() = 0;
+    virtual void move_d() = 0;
+    virtual void move_r() = 0;
+};
