d2ec5b7183ce22ca4916bc0f758206edb6b33660
z399yang <z399yang@student.cs.uwaterloo.ca>
2019-11-30 01:05:08 -0500

    Add state

---
+++ state.h
@@ -0,0 +1,16 @@
+#include <vector> // added as a syntax fix
+#include <cstddef>
+
+enum class PieceType { IBlock, JBlock, LBlock, TBlock, ZBlock, SBlock, OBlock};
+enum class FromType { Board, Piece };
+enum class CommandType { RotateCW, RotateCCW, MoveL, MoveD, MoveR };
+
+struct State {
+  size_t base_row, base_col;
+  std::vector<std::vector<bool>> offset;
+  size_t offset_height;
+  size_t offset_width;
+  PieceType piece_type;
+  FromType from_type;
+  CommandType command_type;
+};
