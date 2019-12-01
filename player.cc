7a1dcf250b368a5094e37bcae7a47c09ae12fae7
yjjcao <yjjcao@student.cs.uwaterloo.ca>
2019-12-01 00:18:18 -0500

    player class can now clear this player's score

--- player.cc
+++ player.cc
@@ -1,9 +1,16 @@
 #include "player.h"
 
+// returns this player's score
 int Player::getScore() {
     return this->score;
 }
 
+// increments this player's score by one
 void Player::incrementScore() {
     ++this->score;
+}
+
+// reset this player's score to 0
+void Player::clearScore() {
+    this->score = 0; 
 }
