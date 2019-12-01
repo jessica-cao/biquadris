c32839fa37c19add4e37bc84cd7fe9d5785d7a55
yjjcao <yjjcao@student.cs.uwaterloo.ca>
2019-12-01 03:05:54 -0500

    main with implementations for the barebones stuff we need

--- main.cc
+++ main.cc
@@ -2,13 +2,20 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 #include "player.h"
 #include "levels.h"
 #include "levelZero.h"
+#include "levelOne.h"
+#include "levelTwo.h"
+#include "levelThree.h"
+#include "levelFour.h"
 #include "trie.h"
 #include "textdisplay.h"
 
 using namespace std;
+
+// CALL GRID INIT IN PLAYER
 
 // The highest level you can reach
 const int MAXLEVEL = 4;
@@ -25,9 +32,12 @@ int main(int argc, char *argv[]) {
     // THIS IS ALL SUBJECT TO CHANGE BC IDK WHAT IM DOING; FEEL FREE TO CORRECT JUST LMK!!
 
     string arg;
-    unique_ptr<Player> player1, player2; // ctors need to set default level to 0
-                                // will need a field that points to Level obj, which then specifies the lvl (eg. LevelZero)
-                                // call Piece * pLevel (playerLevel) and string nLevel (numLevel)
+    // ctors need to set default level to 0
+    // will need a field that points to Level obj, which then specifies the lvl (eg. LevelZero)
+    // call Piece * pLevel (playerLevel) and string nLevel (numLevel)
+    unique_ptr<Player> player1{new Player()};
+    unique_ptr<Player> player2{new Player()};
+    
     string nLevel = "0"; // intialized level: THIS MAY BE A PROBLEM LATER DEPENDING ON HOW WE SET LEVELUP AND DOWN
     string fn1 = "sequence1.txt";
     string fn2 = "sequence2.txt";
@@ -58,28 +68,29 @@ int main(int argc, char *argv[]) {
             nLevel = argv[i+1];
             // WHAT WILL BE KEEPING TRACK OF THE LEVELS?
             if (nLevel == "0") {
-                player1->pLevel = new LevelZero; // syntax and stuff here is gonna have to change and is prob wrong :(
-                player2->pLevel = new LevelZero;
+                player1->pLevel = make_unique<LevelZero>(new LevelZero());
+                player2->pLevel = make_unique<LevelZero>(new LevelZero());
 
                 // ignore other levels for now
             /*
-            } else if (nLevel == "1") {
+            } else if (nLevel == 1) {
                 //
-            } else if (nLevel == "2") {
+            } else if (nLevel == 2) {
                 //
-            } else if (nLevel == "3") {
+            } else if (nLevel == 3) {
                 //
-            } else if (nLevel == "4") {
+            } else if (nLevel == 4) {
                 //
                 */
             }
         }
     }
 
-    cin.exceptions(ios::eofbit|ios::failbit);
+    std::cin.exceptions(ios::eofbit|ios::failbit); // why is cin ambiguous here. Jackass.
     // add any additional intializations here
     string cmd; // reads in a command
-    unique_ptr<Piece> p1, p2; // piece pointers for g1 and g2
+    unique_ptr<Piece> p1 {new Piece()}; // piece pointers for player1's grid
+    unique_ptr<Piece> p2 {new Piece()}; // piece pointer for player2's grid
     int countTurns = 0;
     Trie* head = new Trie();
 	head->insertCommands();
@@ -93,17 +104,19 @@ int main(int argc, char *argv[]) {
             player1->pLevel->setFile(fn1);  // to clean later: just set immediately in for loop for args
             player1->pLevel->setFile(fn2);
             
-            p1 = player1->pLevel->create(); // create your piece; this is the current piece that the cmd is acting on
-            player1->theGrid->attach(p1);
-            p2 = player2->pLevel->create(); // create the opponent's piece; the opponent's current piece
-            player2->theGrid->attach(p2);
+            p1 = make_unique<Piece>(player1->pLevel->create()); // create your piece; this is the current piece that the cmd is acting on
+            //set piece
+            player1->theGrid->attach(p1.get());
+            p2 = make_unique<Piece>(player2->pLevel->create()); // create the opponent's piece; the opponent's current piece
+            // set piece
+            player2->theGrid->attach(p2.get());
 
-            cin >> cmd;
+            std::cin >> cmd;
             multiplier = head->parsePrefix(cmd); // check if the command we have has a prefix
 
             // if the cmd doesn't have a prefix, or if someone wants to mess w it
             // the multiplier is set to 1, so the command executes once
-            if (multiplier <= 0) {
+            if (multiplier == 0) {
                 multiplier = 1;
             }
             
@@ -163,18 +176,104 @@ int main(int argc, char *argv[]) {
                 } else if (currComm == "levelup") {
                     int desiredLvl;
                     if (countTurns % 2 == 0) {
-                        desiredLvl = multiplier + stoi(player1->nLevel);
+                        desiredLvl = multiplier + player1->nLevel;
                         if (desiredLvl <= MAXLEVEL) {
-                            player1->nLevel = to_string(desiredLvl);
+                            player1->pLevel.reset();
+                            
+                            if (desiredLvl == 1) {
+                                player1->pLevel = make_unique<LevelOne>(new LevelOne());
+                            } else if (desiredLvl == 2) {
+                                player1->pLevel = make_unique<LevelTwo>(new LevelTwo());
+                            } else if (desiredLvl == 3) {
+                                player1->pLevel = make_unique<LevelThree>(new LevelThree());
+                            } else if (desiredLvl == 4) {
+                                player1->pLevel = make_unique<LevelFour>(new LevelFour());
+                            }
+                            player1->nLevel = desiredLvl;
                         } else {
-                            player1->nLevel = to_string(MAXLEVEL);
+                            if (player1->nLevel != MAXLEVEL) {
+                                player1->pLevel.reset();
+                                player1->pLevel = make_unique<LevelFour>(new LevelFour());
+                            }
+                            
+                            player1->nLevel = MAXLEVEL;
                         }
                     } else {
+                        desiredLvl = multiplier + player2->nLevel;
+                        if (desiredLvl <= MAXLEVEL) {
+                            player2->pLevel.reset();
 
+                            if (desiredLvl == 1) {
+                                player2->pLevel = make_unique<LevelOne>(new LevelOne());
+                            } else if (desiredLvl == 2) {
+                                player2->pLevel = make_unique<LevelTwo>(new LevelTwo());
+                            } else if (desiredLvl == 3) {
+                                player2->pLevel = make_unique<LevelThree>(new LevelThree());
+                            } else if (desiredLvl == 4) {
+                                player2->pLevel = make_unique<LevelFour>(new LevelFour());
+                            }
+                            player2->nLevel = desiredLvl;
+                        } else {
+                            if (player2->nLevel != MAXLEVEL) {
+                                player2->pLevel.reset();
+                                player2->pLevel = make_unique<LevelFour>(new LevelFour());
+                            }
+                            
+                            player2->nLevel = MAXLEVEL;
+                        }
                     }
-                    
-
                 } else if (currComm == "leveldown") {
+                    int desiredLvl;
+                    if (countTurns % 2 == 0) {
+                        desiredLvl = player1->nLevel - multiplier;
+                        if (desiredLvl >= MINLEVEL) {
+                            player1->pLevel.reset();
+                            
+                            if (desiredLvl == 0) {
+                                player1->pLevel = make_unique<LevelZero>(new LevelZero());
+                            } else if (desiredLvl == 1) {
+                                player1->pLevel = make_unique<LevelOne>(new LevelOne());
+                            } else if (desiredLvl == 2) {
+                                player1->pLevel = make_unique<LevelTwo>(new LevelTwo());
+                            } else if (desiredLvl == 3) {
+                                player1->pLevel = make_unique<LevelThree>(new LevelThree());
+                            }
+                            player1->nLevel = desiredLvl;
+                            
+                        } else {
+
+                             if (player1->nLevel != MINLEVEL) {
+                                player1->pLevel.reset();
+                                player1->pLevel = make_unique<LevelZero>(new LevelZero());
+                            }
+                            
+                            player1->nLevel = MINLEVEL;
+                        }
+                    } else {
+                        desiredLvl = player2->nLevel - multiplier;
+                        if (desiredLvl >= MINLEVEL) {
+                            player2->pLevel.reset();
+                            
+                            if (desiredLvl == 0) {
+                                player2->pLevel = make_unique<LevelZero>(new LevelZero());
+                            } else if (desiredLvl == 1) {
+                                player2->pLevel = make_unique<LevelOne>(new LevelOne());
+                            } else if (desiredLvl == 2) {
+                                player2->pLevel = make_unique<LevelTwo>(new LevelTwo());
+                            } else if (desiredLvl == 3) {
+                                player2->pLevel = make_unique<LevelThree>(new LevelThree());
+                            }
+                            player1->nLevel = desiredLvl;
+                            
+                        } else {
+                            if (player2->nLevel != MINLEVEL) {
+                                player2->pLevel.reset();
+                                player2->pLevel = make_unique<LevelZero>(new LevelZero());
+                            }
+                            
+                            player2->nLevel = MINLEVEL;
+                        }
+                    }
                   
                 } else if (currComm == "random") { // no multiplier
 
@@ -183,8 +282,15 @@ int main(int argc, char *argv[]) {
                 } else if (currComm == "sequence") {
 
                 } else if (currComm == "restart") { // no multiplier
-
-                } else if (currComm == "I") {
+                //clear grid, clear score, reset turn to 0, 
+                player1->theGrid->clear(); // clear the first grid
+                player2->theGrid->clear(); // clear the second grid
+                player1->clearScore();
+                player2->clearScore();
+                countTurns = -1; // This makes sure that no matter who calls restart, player one always plays first
+                
+                } else if (currComm == "I") { // replace current block w the I block
+                
 
                 } else if (currComm == "J") {
 
@@ -200,12 +306,16 @@ int main(int argc, char *argv[]) {
 
                 }
 
+                // TODO print the board right here
+                
+
                 // check for victory condition here 
                 // if victory, then break
                 
+
                 multiplier = 1;
                 ++countTurns;
-            } catch (logic_error &le) {
+            } catch (logic_error &le) { // any invalid command prints an error message
                 cout << le.what() << endl;
             }
             
