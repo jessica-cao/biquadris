60e71af52d8992773c662583c362f9a5818405bd
Jessica Cao <39662320+jessica-cao@users.noreply.github.com>
2019-12-03 22:15:35 -0500

    Delete graphicsdisplay.d

--- graphicsdisplay.d
+++
@@ -1,3 +0,0 @@
-graphicsdisplay.o: graphicsdisplay.cc graphicsdisplay.h player.h grid.h \
- observer.h subject.h info.h state.h levelZero.h levels.h pieces.h \
- levelOne.h levelTwo.h levelThree.h levelFour.h window.h
